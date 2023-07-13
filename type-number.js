eaaddba8fa64d6bc3a27f9b71eccc76c9c246f71
Jean Michell Ritter <michelld777@gmail.com>
2023-07-13 20:24:19 -0300

    Exemplos Aula 1

---
+++ type-number.js
@@ -0,0 +1,22 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 2;
+const segundoNumero = 1;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (Não é um número)
+
---
+++ type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "Olá, Mundo";
+const texto2 = "Olá, Mundo";
+const senha = "SenhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "O Leo disse 'Oi' ";
+
+console.log(citacao)
+
+// Template String OU Template Literal
