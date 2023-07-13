79209dfe9c3c1dace7a49cb342b09049cd13d8a0
Jean Michell Ritter <michelld777@gmail.com>
2023-07-13 20:54:23 -0300

    Códigos Var e Conversões

---
+++ boolean.js
@@ -0,0 +1,22 @@
+// Boolean 
+
+const usuarioLogado = true 
+const contaPaga = false
+
+// Truthy iy Falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+
+// Null
+// Undefined
+
+let minhaVar;
+let varNull = null
+
+console.log(minhaVar);
+console.log(varNull);
---
+++ conversoes.js
@@ -0,0 +1,20 @@
+// Tipo de Dado
+// Booleanos
+
+// Conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+// Number()
+// String()
+
+console.log(numero == numeroString);
+
+console.log(numero + Number(numeroString));
+
+
+const numeroString1 = "456a"
+console.log(numero + Number(numeroString1))
+
+//Conversão Explícita
